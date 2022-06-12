637108da5430ba80b72ebbf807e59b42e8b4aeda
asyx23 <105804344+asyx23@users.noreply.github.com>
2022-06-12 23:10:13 +0200

    Update PixelFilter Method process() und display()

--- src/PixelFilter.java
+++ src/PixelFilter.java
@@ -1,16 +1,29 @@
+import java.awt.BorderLayout;
+import java.awt.Window;
 import java.awt.image.*;
+
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 public abstract class PixelFilter implements Filter {
+	
+	private static JFrame frame;
+	private static JLabel label;
+	
 
     public abstract int calculate(int pixelColor);
-
-    public BufferedImage process (BufferedImage image1, BufferedImage image2){
+    
+    
+    public BufferedImage process (BufferedImage image1){
         // TODO process() schreiben
-        
+    	
         BufferedImage image = null;
 
         try{
@@ -21,9 +34,23 @@ public abstract class PixelFilter implements Filter {
             //TODO Auto-generated
             e.printStackTrace();
         }
-        
-        //test test test
-//        hshdhasdxycyxyddfddfdfdf
-       // dfdf
+         
+        return image;
     }
+    
+    public static void display(BufferedImage image) {
+    	if(frame == null) {
+    	JFrame frame = new JFrame();
+    	JLabel label = new JLabel();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        label.setIcon(new ImageIcon(image));
+        frame.getContentPane().add(label,BorderLayout.CENTER);
+        frame.setLocationRelativeTo(null);
+    	frame.pack();
+    	frame.setVisible(true);
+    	}else label.setIcon(new ImageIcon(image));
+    	
+    }
+    
 }
