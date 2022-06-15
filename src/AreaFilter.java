ec551e77ed3dc655286c0424c5470ef485ede8c5
ThiloWittmer <thilo.wittmer@gmail.com>
2022-06-15 15:54:42 +0200

    Areafilter process() angefangen

--- src/AreaFilter.java
+++ src/AreaFilter.java
@@ -4,9 +4,19 @@ public abstract class AreaFilter implements Filter {
  
     public abstract int calculate(int[] pixel, int[] maskPixel, int index, int width, int height);
 
-    public BufferedImage process(BufferedImage image1, BufferedImage image2) {
-		return image1;
-        // TODO process() schreiben
+    public BufferedImage process(String filterParam, BufferedImage ... image) {
+		int width = image[0].getWidth();
+        int heigth = image[0].getHeight();
+
+        int[] img = (image.length > 0) ? image[0].getRGB(0, 0, width, heigth, null, 0, width) : null;
+        int[] mask = (image.length > 1) ? image[1].getRGB(0, 0, width, heigth, null, 0, width) : null;
+
+        int radius = Integer.valueOf(filterParam);
+
+        // TODO processing Area-Filter
+        
+        image[0] .setRGB(0, 0, width, heigth, img, 0, width);
+        return image[0];
     }
 
 }
