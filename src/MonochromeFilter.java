613d8ff6fc4ae164a1de90bce447378df337cc9c
asyx23 <info@aaron-stier.de>
2022-06-15 18:02:52 +0200

    MonochromeFitler update

--- src/MonochromeFilter.java
+++ src/MonochromeFilter.java
@@ -8,38 +8,11 @@ import javax.imageio.ImageIO;
 public final class MonochromeFilter extends PixelFilter {
 
 	
-	@Override
-	public BufferedImage process() {
-		
-    	BufferedImage image = null;
 
-        try{
-            image = ImageIO.read(new File("input_image.bmp"));
-            
-            calculate(image);
-            
-       
-            ImageIO.write(image, "bmp", new File ("output_image.bmp"));
-        } catch(IOException e){
-            //TODO Auto-generated
-            e.printStackTrace();
-        }
-		return image;
-	
-	}
 
     @Override
-    public BufferedImage calculate(BufferedImage img) {
+    public int calculate(int pixelColor, String param) {
     	
-    	img = process();
-    	
-    	int[] pixel;
-    	
-    	
-    	
-    	return img;
-		
-        // TODO Auto-generated method stub
     
     }
 }   
