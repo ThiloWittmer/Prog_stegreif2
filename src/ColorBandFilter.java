9200682d9d4d2c208083d088119634b58ddc1d00
muhanad <muhanadkhatib998@gmail.com>
2022-06-16 16:33:15 +0200

    Colorband

--- src/ColorBandFilter.java
+++ src/ColorBandFilter.java
@@ -3,15 +3,31 @@ import java.awt.image.BufferedImage;
 public class ColorBandFilter extends PixelFilter {
 
     @Override
-    public BufferedImage process() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    public int calculate(int pixelColor,String filterParam ) {
+     
+        int rot =(pixelColor >>16) & 0xff;
+        int gruen =(pixelColor >>8) & 0xff;
+        int blau =pixelColor & 0xff ;
 
-    @Override
-    public int calculate(int pixelColor) {
-        // TODO Auto-generated method stub
-        return 0;
+      
+
+        switch(filterParam){
+            case "RED":
+                gruen = 0;
+                blau = 0;
+                break;
+            case "BLUE":
+                rot = 0;
+                gruen = 0;
+                break;
+            case "GREEN":
+                rot = 0;
+                blau = 0;
+                break;
+            }
+        
+        pixelColor =  (rot << 16) | (gruen << 8) | blau;
+		return pixelColor;
     }
     
 }
