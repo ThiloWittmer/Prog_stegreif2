c6fb610754f44f0a4072a568005313e5798ad0dc
muhanad <muhanadkhatib998@gmail.com>
2022-06-16 18:57:04 +0200

    MultiTresh-klasse-änderungen

--- src/MultiTreshhold.java
+++ src/MultiTreshhold.java
@@ -1,11 +1,43 @@
-import java.awt.image.BufferedImage;
 
-public class MultiTreshhold extends PixelFilter implements Filter{
+public class MultiTreshhold extends PixelFilter{
 
-    public int calculate(int pixelColor,String filterParam ) {
+    public int calculate(int pixelColor) {
 
-    int rot =(pixelColor >>16) & 0xff;
-    int gruen =(pixelColor >>8) & 0xff;
-    int blau =pixelColor & 0xff ;
-    }
+        int rot =(pixelColor >>16) & 0xff;
+        int gruen =(pixelColor >>8) & 0xff;
+        int blau =pixelColor & 0xff ;
+    
+        int aktHelligkeit = (rot + gruen + blau) / 3; //für die reduzierung der Helligkeit
+         
+        final int SCHWELLWERT, SCHWELLWERT2, SCHWELLWERT3;
+        SCHWELLWERT  =64;
+        SCHWELLWERT2 =128;
+        SCHWELLWERT3 =192;
+
+        final int  GRAUTON1=0;
+        final int  GRAUTON2=92;
+        final int  GRAUTON3=160;
+        final int  GRAUTON4=255;
+
+
+            if(aktHelligkeit >= SCHWELLWERT3){
+                aktHelligkeit = GRAUTON4;
+            }else if( aktHelligkeit >= SCHWELLWERT2){
+                aktHelligkeit = GRAUTON3;
+            }else if( aktHelligkeit >= SCHWELLWERT){
+                aktHelligkeit = GRAUTON2;
+            }
+            else{
+                aktHelligkeit=GRAUTON1 ;
+            }
+
+            pixelColor = 0;
+            
+            pixelColor |=  (aktHelligkeit << 16);
+            pixelColor |= (aktHelligkeit << 8);
+            pixelColor |= aktHelligkeit;
+            return pixelColor;
+    
+    
+        }
 }    
--- src/ThresholdFilter.java
+++ src/ThresholdFilter.java
@@ -2,16 +2,29 @@ import java.awt.image.BufferedImage;
 
 public class ThresholdFilter extends PixelFilter {
 
-    @Override
-    public BufferedImage process() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
     public int calculate(int pixelColor) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+
+        int rot =(pixelColor >>16) & 0xff;
+        int gruen =(pixelColor >>8) & 0xff;
+        int blau =pixelColor & 0xff ;
+    
+        int Grautoene = (rot + gruen + blau) / 3; //für die reduzierung der Helligkeit
+         
+        int Graustufe= 128;
+
+            if(Grautoene>= Graustufe){
+                Grautoene=255;
+            }else{
+                Grautoene=0;
+            }
+            pixelColor = 0;
+            
+            pixelColor |=  (Grautoene << 16);
+            pixelColor |= (Grautoene << 8);
+            pixelColor |= Grautoene;
+            return pixelColor;
+    
+    
+        }
     
 }
