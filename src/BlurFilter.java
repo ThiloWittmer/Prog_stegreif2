dcf01c4a595a1ce6e4ecf4e8c0e82f87898a0fbe
muhanad <muhanadkhatib998@gmail.com>
2022-06-16 22:13:17 +0200

    doku für BlurFilter

--- src/BlurFilter.java
+++ src/BlurFilter.java
@@ -1,13 +1,24 @@
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+/**
+ * BlurFilter(Weichzeichnungsfilter)
+ * 
+ * 
+ * Auslösung der Konturen des Bildes mit einem angegebenen Radius 
+ */
 
 public class BlurFilter extends AreaFilter {
-
+    
+    // geht das Bild durch und ruft der Calculate-Methode auf
     @Override
     public BufferedImage process() {
         // TODO Auto-generated method stub
         return null;
     }
-
+     
+    // Berechnung der Pixeln von dem Bild
     @Override
     public int calculate(int[] pixel, int[] maskPixel, int index, int width, int height) {
         // TODO Auto-generated method stub
