20c48668565a8becfd300cfa12b040af7f1d24a1
ThiloWittmer <thilo.wittmer@gmail.com>
2022-06-16 19:17:22 +0200

    Blurfilter

--- src/BlurFilter.java
+++ src/BlurFilter.java
@@ -1,17 +1,64 @@
-import java.awt.image.BufferedImage;
 
 public class BlurFilter extends AreaFilter {
+    private int radius;
 
-    @Override
-    public BufferedImage process() {
-        // TODO Auto-generated method stub
-        return null;
+    public BlurFilter(int radius) {
+        this.radius = radius;
     }
 
     @Override
-    public int calculate(int[] pixel, int[] maskPixel, int index, int width, int height) {
-        // TODO Auto-generated method stub
-        return 0;
+    public int calculate(int[] pixel, int[] maskPixel, int index, int width, int height) throws IndexOutOfBoundsException {
+        int avgR = 0;
+        int avgG = 0;
+        int avgB = 0;
+        int tmpIndex = index;
+        int tmpWidth = radius / 2;
+        int count = 0;
+
+        for (int i = 0; i < (radius/2+1); i++) {
+            for (int j = 0; j < (tmpWidth*2 + 1); j++) {
+                try {
+                    avgR += (pixel[tmpIndex-tmpWidth + j] >> 16) & 0xFF;
+                    avgG += (pixel[tmpIndex-tmpWidth + j] >> 8) & 0xFF;
+                    avgB += pixel[tmpIndex-tmpWidth + j] & 0xFF;
+                    count++;
+                } catch (IndexOutOfBoundsException e) {
+                    break;
+                }
+            }
+            tmpIndex -= width; 
+            tmpWidth--;
+        }
+        
+        tmpIndex = index + width;
+        tmpWidth = radius/2 - 1;
+        
+        for (int i = 0; i < (radius/2); i++) {
+            for (int j = 0; j < (tmpWidth*2 + 1); j++) {
+                try {
+                    avgR += (pixel[tmpIndex-tmpWidth + j] >> 16) & 0xFF;
+                    avgG += (pixel[tmpIndex-tmpWidth + j] >> 8) & 0xFF;
+                    avgB += pixel[tmpIndex-tmpWidth + j] & 0xFF;
+                    count++;
+                } catch (IndexOutOfBoundsException e) {
+                    e.printStackTrace();
+                }
+            }
+            tmpIndex += width;
+            tmpWidth--;
+        }
+
+        avgR = Math.round((float) avgR / count);
+        avgG = Math.round((float) avgG / count);
+        avgB = Math.round((float) avgB / count);
+
+        pixel[index] = 0;
+
+        pixel[index] |= avgR << 16;
+        pixel[index] |= avgG << 8;
+        pixel[index] |= avgB;
+
+        return pixel[index];
     }
     
 }
