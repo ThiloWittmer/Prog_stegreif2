27628c9214ed9a30582a95372dd640c3456b0c7e
asyx23 <info@aaron-stier.de>
2022-06-16 19:34:15 +0200

    threshold variabel aber funktional

--- src/Control.java
+++ src/Control.java
@@ -8,20 +8,23 @@ public class Control {
 	public static void main(String[] args) {
 		// nur ein Testlauf:
 		BufferedImage image = null;
-		BufferedImage mask = null;
-		Color c = new Color(0xb5cbd9);
-		ColorReplacementFilter filter = new ColorReplacementFilter(c);
-		PixelGraphicFilter filter2 = new PixelGraphicFilter(10);
-		ColorBandFilter filter3 = new ColorBandFilter("RED");
-
+		//BufferedImage mask = null;
+		//Color c = new Color(0xb5cbd9);
+		//ColorReplacementFilter filter = new ColorReplacementFilter(c);
+		//PixelGraphicFilter filter2 = new PixelGraphicFilter(10);
+		//ColorBandFilter filter3 = new ColorBandFilter("RED");
+		
+		int wert = 64;
+		ThresholdFilter filter = new ThresholdFilter(wert);
+		
 		try {
 			image = ImageIO.read(new File("./bilder/test_image.bmp"));
-			mask = ImageIO.read(new File("./bilder/mask.bmp"));
+			//mask = ImageIO.read(new File("./bilder/mask.bmp"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		image = filter3.process(image);
+		image = filter.process(image);
 		
 		try {
 			ImageIO.write(image, "bmp", new File("./bilder/output.bmp"));
--- src/ThresholdFilter.java
+++ src/ThresholdFilter.java
@@ -1,9 +1,11 @@
 import java.awt.image.BufferedImage;
 
 public class ThresholdFilter extends PixelFilter {
+	
+	protected int schwellwert;
 
-	public ThresholdFilter() {
-		
+	public ThresholdFilter(int wert) {
+		this.schwellwert = wert;
 	}
 
     @Override
@@ -17,24 +19,19 @@ public class ThresholdFilter extends PixelFilter {
     	
     	int average = (r + g + b) / 3;
     	
-    	int threshold_white_black = 128;
-    	int threshold_white = 64;
-    	int threshold_grey = 128;
-    	int threshold_black = 192;
-    	
-    	if (average >= threshold_white_black) {
+    	if (average >= schwellwert) {
     		average = 255; 
     	}else {
     		average = 0;
     	}
     	
     	
+    	pixelColor |= average << 16;
+    	pixelColor |= average << 8;
+    	pixelColor |= average;
     	
     	
-    	
-    	
-    	
-        return 0;
+        return pixelColor;
     }
     
 }
